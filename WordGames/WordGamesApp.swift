4143e071da24858c6c5077c5093a653d821addf3
Dylan Conklin <74876741+dylanconklin@users.noreply.github.com>
2023-08-19 19:28:01 -0700

    Initial Commit

---
+++ WordGames/WordGamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGamesApp.swift
+//  WordGames
+//
+//  Created by Dylan Conklin on 8/19/23.
+//
+
+import SwiftUI
+
+@main
+struct WordGamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
