a57f40f0907327e145f43e3ee9e6a82e88a5e58c
Dylan Conklin <74876741+dylanconklin@users.noreply.github.com>
2023-08-20 17:16:53 -0700

    Converted old Eldrow game to WordGames

--- WordGames/ContentView.swift
+++ WordGames/ContentView.swift
@@ -1,24 +1,141 @@
 //
 //  ContentView.swift
-//  WordGames
+//  Word Games
 //
-//  Created by Dylan Conklin on 8/19/23.
+//  Created by Dylan Conklin on 7/31/23.
 //
 
 import SwiftUI
 
+extension String {
+    subscript(index: Int) -> String {
+        String(dropLast(count - (index + 1)).dropFirst(index))
+    }
+}
+
 struct ContentView: View {
+    @State var wordleLength: Int = 7
+    @State private var wordSet: Set<String> = []
+    @State private var wordle: String = ""
+    @State private var guess: String = ""
+    @State private var attemptedWordles: [String] = []
+    @State private var score: Int = 0
+    @State private var limit: Int = 5
+    
+    @State private var errorTitle: String = ""
+    @State private var errorMessage: String = ""
+    @State private var showingError: Bool = false
+
+    func initGame () {
+        if let startWords: String = try? String(contentsOf: Bundle.main.url(forResource: "\(wordleLength)", withExtension: "txt")!) {
+            wordSet = Set(startWords.components(separatedBy: .newlines))
+            newGame()
+        }
+    }
+    
+    func newGame () {
+        wordle = wordSet.randomElement()!
+        guess = ""
+        attemptedWordles = []
+    }
+    
+    func addNewWord() {
+        let result: String = guess.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        if isValid(word: result) {
+                attemptedWordles.insert(result, at: 0)
+                if attemptedWordles.contains(wordle) {
+                    score += result.count
+                    newGame()
+                }else if attemptedWordles.count > limit {
+                    error("You Lose", "The wordle was \(wordle).")
+                    newGame()
+                }
+        }
+        guess = ""
+    }
+
+    func isValid(word: String) -> Bool {
+        var result: Bool = false
+        checker: if word.isEmpty {
+            break checker
+        } else if word.count < wordle.count {
+            error("Invalid Word", "Your guess is shorter than the Wordle.")
+            break checker
+        } else if word.count > wordle.count {
+            error("Invalid Word", "Your guess is longer than the Wordle.")
+            break checker
+        } else if !wordSet.contains(word) {
+            error("Invalid Word", "Your guess is not a real word.")
+            break checker
+        } else {
+            result = true
+        }
+        return result
+    }
+    
+    func error(_ title: String, _ message: String) {
+        errorTitle = title
+        errorMessage = message
+        showingError = true
+    }
+
     var body: some View {
+//        NavigationView {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Wordle")
+                .font(Font.largeTitle)
+            if score > 0 {
+                Text("Score \(score)")
+                    .font(Font.headline)
+            }
+            List {
+                Section {
+                    TextField("Enter your word: ", text: $guess)
+                        .fontDesign(.monospaced)
+                        .autocorrectionDisabled(true)
+                }
+                Stepper("Wordle Length: \(wordleLength)", value: $wordleLength, in: 1...22) {_ in 
+                    initGame()
+                }
+                Button("Get new Word") {
+                    newGame()
+                }
+                Section {
+                    ForEach(attemptedWordles.reversed(), id: \.self) { word in
+                        HStack {
+                            ForEach(Array(word.enumerated()), id: \.offset) { index, letter in
+                                if wordle[index] == String(letter) {
+                                    Text(String(letter))
+                                        .fontDesign(.monospaced)
+                                        .foregroundStyle(Color.green)
+                                } else if wordle.contains(String(letter)) {
+                                    Text(String(letter))
+                                        .fontDesign(.monospaced)
+                                        .foregroundStyle(Color.yellow)
+                                } else {
+                                    Text(String(letter))
+                                        .fontDesign(.monospaced)
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+//            .navigationTitle(wordle)
+            .onSubmit(addNewWord)
+            .onAppear(perform: initGame)
+            .alert(errorTitle, isPresented: $showingError) {
+                Button("OK", role: .cancel) {}
+            } message: {
+                Text(errorMessage)
+            }
         }
         .padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
